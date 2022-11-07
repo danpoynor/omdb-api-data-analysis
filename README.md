6766ee01e5dae3d01c15d8962e9a611b354eb883
Dan Poynor <danpoynor@gmail.com>
2022-11-07 12:50:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,145 @@
+# Open Movie Database (OMDb) API Data Analysis
+
+Gathers data for Oscar-winning movies using their IMDB ids, saves the information to a CSV file, and answers a few data analysis questions about the movies using JupyterLab.
+
+Google Slides presentation is available at: <https://docs.google.com/presentation/d/1n8Oj3vqyDM5oQEsC2xx6O5cvqLt-SElyxxox1CRYDPM/edit?usp=sharing>
+
+---
+
+## Files Included
+
+* `requirements.txt` - Keeps track of modules used so other developers can run the project.
+* `.env.example`- Should be renamed `.env` and filled out with your own API key.
+* `oscar_winners.csv` - CSV file containing Movie Titles an dIMDb IDs to be used in analysis.
+* `movie_requests.py` - Used to access data and creates a new CSV file named `movies.csv` automatically.
+* `movie_analysis` - A Jupyter Notebook containing the analysis.
+
+---
+
+## Data Fields
+
+* Movie Title (string)
+* Runtime (integer)
+* Genre (string)
+* Award Wins (integer)
+* Award Nominations (integer)
+* Box Office (integer)
+* Rated
+* Director
+* Released
+* Language
+
+---
+
+## Analysis Questions
+
+1. What was the runtime for each movie?
+2. What movie had the longest runtime?
+3. Is there any relationship between the length of the movie (runtime) and the number of awards a movie won?
+4. How many awards and nominations did each movie receive?
+5. Is there a relationship between the amount of box office earnings a movie had and the amount of total nominations the movie received (total nominations = awards wins + award nominations)?
+6. Is there a relationship between box office earnings and movie runtimes?
+7. How much did each movie earn at the box office?
+8. What is the total count of each genre present in the dataset? (How many times does fantasy, drama, adventure, etc. show up)
+
+---
+
+## Run the app
+
+Clone this repo then `cd omdb-api-data-analysis`.
+
+Assuming you have Python3 installed on a MacOS, run these commands (or something similar):
+
+```bash
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+👉 Rename the `.env.example` file to `.env` and add your [OMDb API](https://www.omdbapi.com/apikey.aspx):
+
+```bash
+cp .env.example .env
+```
+
+Then run the script to load the data:
+
+```bash
+python movie_requests.py
+```
+
+This will read the file `oscar_winners.csv` and gather data on the listed movies from [OMDb](https://www.omdbapi.com/) before creating a new file called `movies.csv` which will be used for the Jupyter Notebook data analysis.
+
+You can then open the Jupyter Notebook and run the cells to see the analysis.
+
+When done running the app, you can deactivate the virtual environment by running `deactivate`.
+
+---
+
+## Dev Notes
+
+Initial project development process:
+
+1. Create a new repo on GitHub.
+2. Clone the repo to my local machine.
+
+```bash
+git clone <repo_url>
+```
+
+3. `cd` into the repo
+
+```bash
+cd <repo_name>
+```
+
+4. Create a new virtual environment and activate it.
+
+```bash
+python3 -m venv env
+source env/bin/activate
+```
+
+5. Install the required packages.
+
+```bash
+pip install requests
+pip install python-dotenv
+```
+
+6. Create a `requirements.txt` file so that other developers can install the required packages.
+
+```bash
+pip freeze > requirements.txt
+```
+
+7. Create a `.gitignore` file to ignore certain files and folders from being tracked by Git.
+
+```bash
+# .gitignore
+.env
+env/
+```
+
+8. Create a `.env` file to store environment variables.
+
+```bash
+# .env
+OMDB_API_KEY=<your_api_key>
+```
+
+9. Create a `movie_requests.py` file to make requests to the OMDb API and save the data to a CSV file.
+
+10. Create a `oscar_winners.csv` file to store the movie titles and IMDB IDs to be used in the analysis.
+
+11. Create a `movie_analysis.ipynb` file to analyze the data.
+
+12. Create a `README.md` file to document the project.
+
+13. Push the project to GitHub.
+
+```bash
+git add .
+git commit -m "Initial commit"
+git push origin master
+```
---
+++ movie_requests.py
@@ -0,0 +1,133 @@
+"""Request movie data from the OMDB API and write it to a CSV file.
+
+Raises:
+    ValueError: Test if API_KEY is None or empty.
+    ValueError: Test if IMDB ID is empty.
+    ValueError: Test if CSV file is empty.
+    csv.Error: Test if CSV file has invalid headers.
+    csv.Error: Test if CSV file has no rows.
+"""
+import csv
+import os
+
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables into the os environment
+load_dotenv()
+
+# Get the API key from the environment variable
+API_KEY = os.environ.get('OMDB_API_KEY')
+
+# Potential TODOs:
+# Clean the received data
+# Check for duplicates
+# Add a progress bar rather than printing each Title as processed
+# Handle more potential errors
+
+# Reference DOCs:
+# requests: https://requests.readthedocs.io/en/latest/
+# dotenv: https://github.com/theskumar/python-dotenv
+
+
+def get_movie_data(imdb):
+    """Request data from the OMDB API with the provided IMDB id.
+
+    Args:
+        imdb (str): ID from IMDB to request data for.
+
+    Returns:
+        requests.models.Response: Response object formatted as JSON dictionary.
+    """
+    # Make sure IMDB id is not empty
+    if imdb is None:
+        raise ValueError("IMDB ID not set")
+
+    r = requests.get(f'https://www.omdbapi.com/?apikey={API_KEY}&i={imdb}')
+    return r.json()
+
+
+def create_csv_file(data):
+    """Write the data to a CSV file named `movies.csv`.
+
+    Args:
+        data (list): List of dictionaries containing data to write to a CSV file.
+    """
+    with open('movies.csv', 'w', newline='') as csvfile:
+        # Create a sequence of keys to identify order of values to be written
+        fieldnames = ['Title', 'Runtime', 'Genre', 'Awards', 'BoxOffice', 'Rated', 'Director', 'Year', 'Language']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for movie in data:
+            # For each value in fieldnames, get the value then write the row
+            writer.writerow({field: movie[field] for field in fieldnames})
+
+            # writer.writerow(movie_info)
+    print(f'CSV file movies.csv created and containing {len(data)} movies.')
+    print('-'*79)
+
+
+def main():
+    """Get data for each movie in the `oscar_winners.csv` CSV file."""
+
+    # Test API_KEY value is not None or empty
+    api_key_value_test = API_KEY
+    if api_key_value_test is None:
+        raise ValueError("API_KEY is None.")
+    elif api_key_value_test == "":
+        raise ValueError("API_KEY is empty.")
+
+    # Assume all's well and continue
+    with open('oscar_winners.csv') as csv_file:
+        fieldnames = ['Movie', 'IMDB']
+        reader = csv.DictReader(csv_file)
+        headers = reader.fieldnames
+
+        # Sanity check to make sure the correct header row is present
+        if reader.fieldnames is None or len(set(fieldnames).intersection(set(headers))) == 0:
+            raise csv.Error("CSV Error: Invalid headers: %s" % str(headers))
+
+        # Check the number of rows is not 0
+        elif reader.line_num == 0:
+            raise csv.Error("CSV Error: No rows in CSV file")
+
+        # Else assume the file is valid and continue
+        else:
+            # Show row count as file is processed
+            # NOTE: If csv file is large, might want to avoid opening it twice
+            # REF: https://stackoverflow.com/questions/2890549/number-of-lines-in-csv-dictreader/2891061#65006592
+            # -1 to skip header
+            total_rows = sum(1 for _ in open('oscar_winners.csv'))
+            print('-'*79)
+            print(f'Processing {total_rows - 1} rows from oscar_winners.csv')
+            print('-'*79)
+
+            movies = []
+            for row in reader:
+                # Get movie data from OMDB API
+                movie_data = get_movie_data(row['IMDB'])
+                # Check received movie title matches the one in the CSV file
+                if movie_data['Title'] == row['Movie']:
+                    print(f"Processing: {row['IMDB']} {movie_data['Title']}")
+                    movies.append(movie_data)
+                else:
+                    # Replace the movie title with the one from the CSV file
+                    # This currently only applies to Birdman but could
+                    # potentially apply to other movies in the future.
+                    print(f"Processing: {row['IMDB']}, {row['Movie']}")
+                    print(f"TITLE MISMATCH: Using OMDb title \"{row['Movie']}\" instead of IMBd \"{movie_data['Title']}\"")
+                    mismatched_movie_data = movie_data
+                    mismatched_movie_data['Title'] = row['Movie']
+                    movies.append(mismatched_movie_data)
+
+            print('-'*79)
+            print('Data retrieved successfully.')
+            print('-'*79)
+
+            # Pass the movies list to the `create_csv_file` function
+            create_csv_file(movies)
+
+
+if __name__ == '__main__':
+    main()
