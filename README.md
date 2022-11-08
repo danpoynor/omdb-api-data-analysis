a0e07cd3d13b43dc8b9e526b4f1b2d6f4767ee56
Dan Poynor <danpoynor@gmail.com>
2022-11-08 15:57:24 -0600

    Get data and create csv file

--- README.md
+++ README.md
@@ -67,3 +67,73 @@ python movie_requests.py
 ```
 
 This will read the file `oscar_winners.csv` and gather data on the listed movies before creating a new file called `movies.csv` which will be used for the Jupyter Notebook data analysis.
+
+When done running the app, you can deactivate the virtual environment by running `deactivate`.
+
+## Dev Notes
+
+Initial project development process:
+
+1. Create a new repo on GitHub.
+2. Clone the repo to my local machine.
+
+```bash
+git clone <repo_url>
+```
+
+3. `cd` into the repo
+
+```bash
+cd <repo_name>
+```
+
+4. Create a new virtual environment and activate it.
+
+```bash
+python3 -m venv env
+source env/bin/activate
+```
+
+5. Install the required packages.
+
+```bash
+pip install requests
+```
+
+6. Create a `requirements.txt` file so that other developers can install the required packages.
+
+```bash
+pip freeze > requirements.txt
+```
+
+7. Create a `.gitignore` file to ignore certain files and folders from being tracked by Git.
+
+```bash
+# .gitignore
+.env
+env/
+```
+
+8. Create a `.env` file to store environment variables.
+
+```bash
+# .env
+OMDB_API_KEY=<your_api_key>
+```
+
+9. Create a `movie_requests.py` file to make requests to the OMDb API and save the data to a CSV file.
+
+10. Create a `oscar_winners.csv` file to store the movie titles and IMDB IDs to be used in the analysis.
+
+11. Create a `movie_analysis.ipynb` file to analyze the data.
+
+12. Create a `README.md` file to document the project.
+
+13. Push the project to GitHub.
+
+```bash
+git add .
+git commit -m "Initial commit"
+git push origin master
+```
+
--- movie_requests.py
+++ movie_requests.py
@@ -1,24 +1,66 @@
-# Install the Python Requests library:
-# `pip install requests`
+# requests docs: https://requests.readthedocs.io/en/latest/
+# dotenv docs: https://github.com/theskumar/python-dotenv
 
-import requests
 import csv
 import os
+
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables into the os environment
+load_dotenv()
 
 # Get the API key from the environment variable
 API_KEY = os.environ.get('OMDB_API_KEY')
 
-# Create a function to request data from the OMDb API for
-# each movie in the CSV file using their IMDB ids.
+# TODO: Clean the data
+# TODO: Check for duplicates
 
-# Save the returned data to a new CSV file named movies.csv. It should include:
-# Movie Title (string)
-# Runtime (integer)
-# Genre (string)
-# Award Wins (integer)
-# Award Nominations (integer)
-# Box Office (integer)
-# Rated
-# Director
-# Released
-# Language
+
+def get_movie_data(IMDB):
+    """Request data from the OMDB API with the provided IMDB id.
+
+    Args:
+        IMDB (string): ID from IMDB to request data for.
+
+    Returns:
+        _type_: _description_
+    """
+    r = requests.get(f'http://www.omdbapi.com/?apikey={API_KEY}&i={IMDB}')
+    return r.json()
+
+
+def create_csv_file(data):
+    """Write the data to a CSV file named `movies.csv`.
+
+    Args:
+        data (_type_): _description_
+    """
+    with open('movies.csv', 'w', newline='') as csvfile:
+        # Create a sequence of keys to identify the order of values to be written
+        fieldnames = ['Title', 'Runtime', 'Genre', 'Awards', 'BoxOffice', 'Rated', 'Director', 'Year', 'Language']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for movie in data:
+            # For each value in fieldnames, get the value then write the row
+            writer.writerow({field: movie[field] for field in fieldnames})
+
+            # writer.writerow(movie_info)
+    print("CSV file movies.csv created")
+
+
+def main():
+    """Get data for each movie in the `oscar_winners.csv` CSV file."""
+    with open('oscar_winners.csv') as csv_file:
+        reader = csv.DictReader(csv_file)
+        movies = []
+        for row in reader:
+            movies.append(get_movie_data(row['IMDB']))
+
+    # Pass the movies list to the `create_csv_file` function
+    create_csv_file(movies)
+
+
+if __name__ == '__main__':
+    main()
